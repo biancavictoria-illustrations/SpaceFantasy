84272aaf874d7e066ce262577e1dd4ad0483e218
portaljumperjen <portaljumperjen@gmail.com>
2021-11-03 15:40:04 -0700

    attempting to use yarn spinner

---
+++ 169Capstone/Assets/Scripts/NPCs/NPC.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public static NPC ActiveNPC {get; private set;}
+
+    public string characterName;
+
+    public string yarnStartNode = "Start";
+    public YarnProgram yarnDialogue;
+
+    public DialogueManager dialogueManager;
+
+    public bool NPCisActive = false;
+
+    void Start()
+    {
+        dialogueManager.dialogueRunner.Add(yarnDialogue);
+
+        // TEMP (cut this after testing)
+        ActiveNPC = true ? this : null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If the player is near this NPC
+        // if()
+        // {
+        //     ActiveNPC = true ? this : null;
+        // }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // If the player is no longer near this NPC
+        // if()
+        // {
+        //     ActiveNPC = false ? this : null;
+        // }
+    }
+}
---
+++ 169Capstone/Assets/Scripts/Player/PlayerInteract.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public bool isInDialogue = false;
+
+    public DialogueManager dialogueManager;
+
+    public void Interact()
+    {
+        // If interact input
+        if(Input.GetButtonDown("space")){
+            // If NPC is active and not already talking
+            if(NPC.ActiveNPC && !isInDialogue){
+                StartDialogue();
+            }
+        }
+    }
+
+    public void StartDialogue()
+    {
+        isInDialogue = true;
+        dialogueManager.dialogueRunner.StartDialogue(NPC.ActiveNPC.yarnStartNode);
+        // Start dialogue, activate UI -> don't let the player move anymore (pause the game, basically)
+    }
+
+    public void EndDialogue()
+    {
+        isInDialogue = false;
+        // End dialogue, close UI -> let the player move (unpause the game)
+    }
+}
