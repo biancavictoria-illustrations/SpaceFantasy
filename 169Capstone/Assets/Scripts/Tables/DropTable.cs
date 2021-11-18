7db993012a7cf7c7d436df05c1e551e2a3bf855b
Sami Terracciano <sterracc@uci.edu>
2021-11-17 19:41:42 -0800

    Drop table scriptable objects

---
+++ 169Capstone/Assets/Scripts/Tables/DropTable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/DropTableObject")]
+public class DropTable : ScriptableObject
+{
+    public List<string> ItemType;
+    public List<string> ItemRarityTier;
+    public List<float> DropChance;
+    [SerializeField] private int bossesRequired;
+}
