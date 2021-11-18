526f28b34fee748178d6c8aa7e1ceb790bcb6bf6
Sami Terracciano <sterracc@uci.edu>
2021-11-17 17:27:18 -0800

    Object manager loads player into Main Hub upon player death

--- 169Capstone/Assets/Scripts/Entity/Enemy/GroundPathing.cs
+++ 169Capstone/Assets/Scripts/Entity/Enemy/GroundPathing.cs
@@ -19,13 +19,18 @@ public class GroundPathing : MonoBehaviour
     void Start()
     {
         agent.isStopped = true;
-        player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        //player = GameObject.FindWithTag("Player").GetComponent<Transform>();
         //agent.speed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(player == null)
+        {
+            player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        }
+        
         float distance = Vector3.Distance(player.position, self.position);
 
         if(distance < attackRadius)
