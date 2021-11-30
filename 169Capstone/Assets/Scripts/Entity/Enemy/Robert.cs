fceb523798062b1be5129e16cd413263171b4d75
Sami Terracciano <sterracc@uci.edu>
2021-11-29 18:44:18 -0800

    Generalized Enemies

---
+++ 169Capstone/Assets/Scripts/Entity/Enemy/Robert.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robert : Enemy
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(path.Provoked() && !coroutineRunning)
+        {
+            coroutineRunning = true;
+            StartCoroutine(RobertLogic());
+        }
+    }
+
+    private IEnumerator RobertLogic()
+    {
+        Timer timer = Instantiate(timerPrefab).GetComponent<Timer>();
+        yield return new WaitUntil(() => path.InAttackRange() && !path.attacking);
+        path.attacking = true;
+
+        for(int i = 0; i < 3; i++)
+        {
+            StartCoroutine(baseAttack.Attack(Instantiate(timerPrefab).GetComponent<Timer>(), false, logic.windUp, logic.duration, logic.windDown, logic.coolDown));
+            StartCoroutine(CallDamage());
+            timer.StartTimer(0.16f);
+            yield return new WaitUntil(() => baseAttack.Completed && timer.timeRemaining <= 0);
+        }
+
+        path.attacking = false;
+        coroutineRunning = false;
+        Destroy(timer);
+        //path.coro
+        //StartCoroutine(baseAttack.Attack(Instantiate(timerPrefab).GetComponent<Timer>(), false, logic.windUp, logic.duration, logic.windDown, logic.coolDown));
+    }
+}
