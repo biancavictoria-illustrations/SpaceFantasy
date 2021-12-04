9382b481b3c10b3cb4ebdf237714707093bdd2f0
portaljumperjen <portaljumperjen@gmail.com>
2021-12-03 17:17:48 -0800

    scene transition door script & movement tweaks

--- 169Capstone/Assets/Scripts/Entity/Player/Movement/Movement.cs
+++ 169Capstone/Assets/Scripts/Entity/Player/Movement/Movement.cs
@@ -5,61 +5,31 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    public Animator animator;
     public float speed = 5;
     public Transform model;
     public CharacterController player;
     public float smoothing = 0.1f;
-    public float gravityAccel = -10f;
-    public float jumpSpeed = 10;
-    
     private float smoothingVelocity;
+    
     private float horizontalMove;
     private float verticalMove;
-    private float fallingVelocity;
-    private bool isGrounded;
-    private bool isJumping;
+
+    public Animator animator;
+
+    private bool movingLeft, movingRight, movingUp, movingDown = false;
     
     void Start()
     {
         InputActionAsset controls = gameObject.GetComponent<PlayerInput>().actions;
 
         // Add STOPPING moving when you're no longer holding the button
-        controls.FindAction("MoveHorizontal").canceled += x => OnMoveHorizontalCanceled();
-        controls.FindAction("MoveVertical").canceled += x => OnMoveVerticalCanceled();
-        controls.FindAction("Jump").canceled += x => OnJumpCanceled();
-        
-        Collider[] colliders = Physics.OverlapSphere(transform.position, 0.1f, LayerMask.GetMask("RoomBounds"));
-
-        foreach(Collider col in colliders)
-        {
-            if(col.bounds.Contains(transform.position))
-            {
-                Room roomScript = col.GetComponent<Room>();
-                if(roomScript != null)
-                    AudioManager.Instance.playMusic(AudioManager.MusicTrack.Level1, roomScript.hasEnemies());
-                break;
-            }
-        }
+        controls.FindAction("MoveLeft").canceled += x => OnMoveLeftCanceled();
+        controls.FindAction("MoveRight").canceled += x => OnMoveRightCanceled();
+        controls.FindAction("MoveUp").canceled += x => OnMoveUpCanceled();
+        controls.FindAction("MoveDown").canceled += x => OnMoveDownCanceled();
     }
-    
+
     void Update()
-    {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, 0.1f, LayerMask.GetMask("RoomBounds"));
-
-        foreach(Collider col in colliders)
-        {
-            if(col.bounds.Contains(transform.position))
-            {
-                Room roomScript = col.GetComponent<Room>();
-                if(roomScript != null)
-                    AudioManager.Instance.toggleCombat(roomScript.hasEnemies());
-                break;
-            }
-        }
-    }
-
-    void FixedUpdate()
     {
         if(!InputManager.instance.CanAcceptGameplayInput()){
             return;
@@ -67,53 +37,112 @@ public class Movement : MonoBehaviour
         HandleMovement();
     }
 
-    public void OnMoveHorizontal(InputValue input)
+    public void OnMoveLeft(InputValue input)
     {
         if(!InputManager.instance.CanAcceptGameplayInput()){
             return;
         }
-        verticalMove = input.Get<float>();
+        movingLeft = true;
+        if(input != null){
+            horizontalMove = input.Get<float>();
+        }
+        else{
+            horizontalMove = -1f;            
+        }
         animator.SetBool("IsRunning", true);
     }
 
-    public void OnMoveHorizontalCanceled()
+    public void OnMoveLeftCanceled()
     {
-        verticalMove = 0;
-        CheckForIdle();
+        movingLeft = false;
+        if(!movingRight){
+            horizontalMove = 0;
+            CheckForIdle();
+        }
+        else{
+            OnMoveRight(null);
+        }
     }
 
-    public void OnMoveVertical(InputValue input)
+    public void OnMoveRight(InputValue input)
     {
         if(!InputManager.instance.CanAcceptGameplayInput()){
             return;
         }
-        horizontalMove = input.Get<float>();
+        movingRight = true;
+        if(input != null){
+            horizontalMove = input.Get<float>();
+        }
+        else{            
+            horizontalMove = 1f;
+        }
         animator.SetBool("IsRunning", true);
     }
 
-    public void OnMoveVerticalCanceled()
+    public void OnMoveRightCanceled()
     {
-        horizontalMove = 0;
-        CheckForIdle();
+        movingRight = false;
+        if(!movingLeft){
+            horizontalMove = 0;
+            CheckForIdle();
+        }
+        else{
+            OnMoveLeft(null);
+        }
     }
 
-    public void OnJump(InputValue input)
+    public void OnMoveUp(InputValue input)
     {
         if(!InputManager.instance.CanAcceptGameplayInput()){
             return;
         }
+        movingUp = true;
+        if(input != null){
+            verticalMove = input.Get<float>();            
+        }
+        else{
+            verticalMove = 1f;
+        }
+        animator.SetBool("IsRunning", true);
+    }
 
-        if(isGrounded)
-        {
-            fallingVelocity = jumpSpeed * Time.fixedDeltaTime;
-            isGrounded = false;
-            isJumping = true;
+    public void OnMoveUpCanceled()
+    {
+        movingUp = false;
+        if(!movingDown){
+            verticalMove = 0;
+            CheckForIdle();
+        }
+        else{
+            OnMoveDown(null);
         }
     }
 
-    public void OnJumpCanceled()
+    public void OnMoveDown(InputValue input)
     {
-        //If we need to put code for what happens when the player lets go of jump
+        if(!InputManager.instance.CanAcceptGameplayInput()){
+            return;
+        }
+        movingDown = true;
+        if(input != null){
+            verticalMove = input.Get<float>();            
+        }
+        else{
+            verticalMove = -1f;
+        }
+        animator.SetBool("IsRunning", true);
+    }
+
+    public void OnMoveDownCanceled()
+    {
+        movingDown = false;
+        if(!movingUp){
+            verticalMove = 0;
+            CheckForIdle();
+        }
+        else{
+            OnMoveUp(null);
+        }
     }
 
     private void CheckForIdle()
@@ -125,62 +154,33 @@ public class Movement : MonoBehaviour
 
     private void HandleMovement()
     {
-        Vector3 direction = new Vector3(-horizontalMove, 0, verticalMove).normalized;
+        Vector3 direction = new Vector3(-verticalMove, 0, horizontalMove).normalized;
 
         if(direction.magnitude >= 0.1f)
         {
             float rad = 45 * Mathf.Deg2Rad;
-            if(horizontalMove > 0)
+            if(verticalMove > 0)
             {
                 direction.z -= rad;
             }
-            else if(horizontalMove < 0)
+            else if(verticalMove < 0)
             {
                 direction.z += rad;
             }
 
-            if(verticalMove > 0)
+            if(horizontalMove > 0)
             {
                 direction.x -= rad;
             }
-            else if(verticalMove < 0)
+            else if(horizontalMove < 0)
             {
                 direction.x += rad;
             }
-
-            direction *= speed * Time.fixedDeltaTime;
             
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(model.eulerAngles.y, targetAngle, ref smoothingVelocity, smoothing);
             model.rotation = Quaternion.Euler(0, angle, 0);
+            player.Move(direction * speed * Time.deltaTime);
         }
-        else
-        {
-            direction = Vector3.zero;
-        }
-
-        if(!isJumping && Physics.Raycast(transform.position, Vector3.down, 0.1f, LayerMask.GetMask("Environment")))
-        {
-            fallingVelocity = -10000;
-            isGrounded = true;
-        }
-        else
-        {
-            if(isGrounded)
-            {
-                fallingVelocity = 0;
-                isGrounded = false;
-            }
-
-            fallingVelocity += gravityAccel * Time.fixedDeltaTime;
-        }
-
-        if(fallingVelocity < 0)
-        {
-            isJumping = false;
-        }
-
-        direction += Vector3.up * fallingVelocity;
-        player.Move(direction);
     }
 }
---
+++ 169Capstone/Assets/Scripts/SceneTransitionDoor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionDoor : MonoBehaviour
+{
+    public static SceneTransitionDoor ActiveDoor {get; private set;}
+
+    [SerializeField] private string goToSceneName;    // Set in the inspector, the room this door LEADS TO
+
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(goToSceneName);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If the collision was caused by the player
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player")){
+            ActiveDoor = this;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // If the collision was caused by the player
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player")){
+            ActiveDoor = null;
+        }
+    }
+}
+
+
+/*
+    TODO: Add to InputManager bottom of OnInteract function
+
+    // If you're in range of a door, walk through it
+    else if(SceneTransitionDoor.ActiveDoor){
+        SceneTransitionDoor.ActiveDoor.ChangeScene();
+    }
+*/
