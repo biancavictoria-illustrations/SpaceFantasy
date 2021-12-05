4b3bb83dcc18a90ffb49dcb22d6cabcae17cf9b8
Sami Terracciano <sterracc@uci.edu>
2021-12-04 19:06:03 -0800

    initial integration

---
+++ 169Capstone/Assets/Scripts/Manager/GearManagerObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/GearManagerObject")]
+public class GearManagerObject : ScriptableObject
+{
+    public GameObject[] weapons;
+    public GameObject[] accessories;
+    public GameObject[] head;
+    public GameObject[] legs;
+}
