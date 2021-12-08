25d422475379c454394d36b750e15444951b917e
Sami Terracciano <sterracc@uci.edu>
2021-12-08 14:55:07 -0800

    Prefab fixes

---
+++ 169Capstone/Assets/Scripts/Utilities/FallTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallTrigger : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Fell onto trigger");
+        if(other.tag == "Player")
+        {
+            EntityHealth playerHealth = other.GetComponent<EntityHealth>();
+            playerHealth.Damage(playerHealth.currentHitpoints);
+        }
+    }
+}
