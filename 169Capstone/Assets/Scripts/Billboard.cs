05a197d182efde83f8f04e6b5f9780bd298c471a
chaseoqueso <chaseoqueso@gmail.com>
2022-01-17 15:27:10 -0800

    Slime animation fix and billboarding tweaks

--- 169Capstone/Assets/Scripts/Billboard.cs
+++ 169Capstone/Assets/Scripts/Billboard.cs
@@ -5,12 +5,30 @@ using UnityEngine;
 public class Billboard : MonoBehaviour
 {
     public Transform spriteHolder;
+    public new SpriteRenderer renderer;
+
+    private Vector3 towardsCamera;
+
+    void Start()
+    {
+        if(!renderer)
+            renderer = GetComponentInChildren<SpriteRenderer>();
+        
+        towardsCamera = -Camera.main.transform.forward;
+
+        Vector3 towardsCameraNoY = new Vector3(towardsCamera.x, 0, towardsCamera.z).normalized;
+        
+        //Adjust the transform to correct for the amount the sprite was tilted
+        float tiltAmount = renderer.sprite.bounds.size.y * Mathf.Cos(Vector3.Angle(towardsCameraNoY, towardsCamera)) / 2;
+        spriteHolder.position += towardsCameraNoY * tiltAmount;
+    }
 
     void Update()
     {
-        if(spriteHolder)
+        if(spriteHolder && renderer)
         {
-            spriteHolder.forward = -Camera.main.transform.forward;
+            //Face the sprite towards the camera
+            spriteHolder.forward = towardsCamera;
         }
     }
 }
