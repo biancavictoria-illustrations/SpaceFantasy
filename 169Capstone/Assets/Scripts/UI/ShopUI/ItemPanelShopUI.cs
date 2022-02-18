d41e5370ee07ecd2bfdd339db53e67f51fbadc79
portaljumperjen <portaljumperjen@gmail.com>
2022-02-18 11:35:35 -0800

    start of Stellan shop UI (he's a shopkeeper again but it doesn't break things this time)

--- 169Capstone/Assets/Scripts/UI/ShopUI/ItemPanelShopUI.cs
+++ 169Capstone/Assets/Scripts/UI/ShopUI/ItemPanelShopUI.cs
@@ -58,7 +58,7 @@ public class ItemPanelShopUI : MonoBehaviour, ISelectHandler, IDeselectHandler, 
         costText.text = "$" + currentCostValue;     // TODO: Change $ to better symbol, or just put the icon on the cards
     }
 
-    private void SetHoverAlertsActive(bool set)
+    public void SetHoverAlertsActive(bool set)
     {
         if(itemCardButton.interactable){
             hoverAlerts.EnableAlert(panelPos, set);
