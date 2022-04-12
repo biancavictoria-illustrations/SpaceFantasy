cfa7ad7074c6db097f0b0ecd90f0feed5bd804e9
portaljumperjen <portaljumperjen@gmail.com>
2022-04-12 00:07:49 -0700

    doctor shop working now???

--- 169Capstone/Assets/Scripts/UI/ShopUI/ShopUIDoctor.cs
+++ 169Capstone/Assets/Scripts/UI/ShopUI/ShopUIDoctor.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class ShopUIDoctor : ShopUI
 {
-    public int upgradeBaseCost;     // Set in inspector
+    public int upgradeBaseCost = 10;
+    private bool haveOpenedShop = false;
 
     public override void OpenShopUI()
     {
@@ -15,10 +16,11 @@ public class ShopUIDoctor : ShopUI
     private void SetShopUIValues()
     {
         // If you haven't yet talk to him this run, generate new values
-        if(!NPC.ActiveNPC.haveTalkedToThisRun){
+        if(!haveOpenedShop){
             foreach(ItemPanelDoctor panel in itemPanels){
                 panel.GenerateNewDoctorUpgradeValues(upgradeBaseCost);
             }
+            haveOpenedShop = true;
         }
         else{   // Otherwise (if you HAVE talked to him already), just update prices/descriptions if necessary
             foreach(ItemPanelDoctor panel in itemPanels){
