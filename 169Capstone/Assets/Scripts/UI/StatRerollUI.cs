c4d02eb21cf6a9900a035c3c0368ee02ec386838
portaljumperjen <portaljumperjen@gmail.com>
2022-04-21 15:54:14 -0700

    stat reroll UI popup FUNCTIONAL (but ugly)

--- 169Capstone/Assets/Scripts/UI/StatRerollUI.cs
+++ 169Capstone/Assets/Scripts/UI/StatRerollUI.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class StatRerollUI : MonoBehaviour
 {
@@ -14,11 +15,13 @@ public class StatRerollUI : MonoBehaviour
     [SerializeField] private TMP_Text CONText;
     [SerializeField] private TMP_Text CHAText;
 
+    [SerializeField] private Button continueButton;
+
     public void EnableStatRerollUI()
     {
         ToggleActiveStatus(true);
 
-        PlayerStats stats = Player.instance.stats;
+        PlayerStats stats = Player.instance.GetComponent<PlayerStats>();
 
         // Set stat values
         STRText.text = "" + stats.Strength();
@@ -27,11 +30,15 @@ public class StatRerollUI : MonoBehaviour
         WISText.text = "" + stats.Wisdom();
         CONText.text = "" + stats.Constitution();
         CHAText.text = "" + stats.Charisma();
+
+        continueButton.Select();
     }
 
     public void DisableStatRerollUI()
     {
         ToggleActiveStatus(false);
+
+        // TODO: Set an alert like "<key> Open Inventory" that disappears after X seconds
     }
 
     private void ToggleActiveStatus(bool set)
