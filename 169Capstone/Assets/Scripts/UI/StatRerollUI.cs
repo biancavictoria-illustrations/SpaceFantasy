bc087e04b80be8e0a6f8c2c956838a91d825f47d
portaljumperjen <portaljumperjen@gmail.com>
2022-04-26 12:57:51 -0700

    load screen UI assets; stat reroll juice started

--- 169Capstone/Assets/Scripts/UI/StatRerollUI.cs
+++ 169Capstone/Assets/Scripts/UI/StatRerollUI.cs
@@ -17,21 +17,42 @@ public class StatRerollUI : MonoBehaviour
 
     [SerializeField] private Button continueButton;
 
+    [Tooltip("The number of seconds a given number stays on the screen during the reroll animation.")]
+    [SerializeField] private float statAnimationNumberDuration = 1f;
+    [Tooltip("The counter that ticks down to completing the animation for the first stat (each die completes one after the next). Total duration in sec is statAnimationNumberDuration*this value")]
+    [SerializeField] private int numberOfRollAnimations = 20;
+    // [Tooltip("")]
+    [SerializeField] private int addedToEachNextDuration = 5;
+
+    private PlayerStats stats;
+    private PermanentUpgradeManager upgradeManager;
+
     public void EnableStatRerollUI()
     {
         ToggleActiveStatus(true);
 
-        PlayerStats stats = Player.instance.GetComponent<PlayerStats>();
+        // Don't let us leave until the animation is done
+        continueButton.interactable = false;
 
-        // Set stat values
-        STRText.text = "" + stats.Strength();
-        DEXText.text = "" + stats.Dexterity();
-        INTText.text = "" + stats.Intelligence();
-        WISText.text = "" + stats.Wisdom();
-        CONText.text = "" + stats.Constitution();
-        CHAText.text = "" + stats.Charisma();
+        // For convenience, save these locally
+        stats = Player.instance.GetComponent<PlayerStats>();
+        upgradeManager = PermanentUpgradeManager.instance;
 
-        continueButton.Select();
+        // Set starting values
+        STRText.text = "" + RandomNumberInStatRange( upgradeManager.strMin, upgradeManager.strMax );
+        DEXText.text = "" + RandomNumberInStatRange( upgradeManager.dexMin, upgradeManager.dexMax );
+        INTText.text = "" + RandomNumberInStatRange( upgradeManager.intMin, upgradeManager.intMax );
+        WISText.text = "" + RandomNumberInStatRange( upgradeManager.wisMin, upgradeManager.wisMax );
+        CONText.text = "" + RandomNumberInStatRange( upgradeManager.conMin, upgradeManager.conMax );
+        CHAText.text = "" + RandomNumberInStatRange( upgradeManager.charismaMin, upgradeManager.charismaMax );
+        
+        // Start the couroutines for everything
+        StartCoroutine(RerollAnimationRoutine(PlayerFacingStatName.STR, numberOfRollAnimations));
+        StartCoroutine(RerollAnimationRoutine(PlayerFacingStatName.DEX, numberOfRollAnimations + addedToEachNextDuration));
+        StartCoroutine(RerollAnimationRoutine(PlayerFacingStatName.INT, numberOfRollAnimations + addedToEachNextDuration*2));
+        StartCoroutine(RerollAnimationRoutine(PlayerFacingStatName.WIS, numberOfRollAnimations + addedToEachNextDuration*3));
+        StartCoroutine(RerollAnimationRoutine(PlayerFacingStatName.CON, numberOfRollAnimations + addedToEachNextDuration*4));
+        StartCoroutine(RerollAnimationRoutine(PlayerFacingStatName.CHA, numberOfRollAnimations + addedToEachNextDuration*5));
     }
 
     public void DisableStatRerollUI()
@@ -46,5 +67,55 @@ public class StatRerollUI : MonoBehaviour
         statRerollPanel.SetActive(set);
         GameManager.instance.statRerollUIOpen = set;
         InputManager.instance.RunGameTimer(!set,!set);
+    }
+
+    // Called once the animation is complete to set the actual value you rolled
+    private void SetActualStatValues()
+    {
+        STRText.text = "" + stats.Strength();
+        DEXText.text = "" + stats.Dexterity();
+        INTText.text = "" + stats.Intelligence();
+        WISText.text = "" + stats.Wisdom();
+        CONText.text = "" + stats.Constitution();
+        CHAText.text = "" + stats.Charisma();
+
+        // TODO: Flourish
+
+        continueButton.interactable = true;
+        continueButton.Select();
+    }
+
+    private IEnumerator RerollAnimationRoutine( PlayerFacingStatName stat, int counter )
+    {
+        while(counter > 0){
+            yield return new WaitForSecondsRealtime(statAnimationNumberDuration);
+            --counter;
+            switch(stat){
+                case PlayerFacingStatName.STR:
+                    STRText.text = "" + RandomNumberInStatRange( upgradeManager.strMin, upgradeManager.strMax );
+                    break;
+                case PlayerFacingStatName.DEX:
+                    DEXText.text = "" + RandomNumberInStatRange( upgradeManager.dexMin, upgradeManager.dexMax );
+                    break;
+                case PlayerFacingStatName.INT:
+                    INTText.text = "" + RandomNumberInStatRange( upgradeManager.intMin, upgradeManager.intMax );
+                    break;
+                case PlayerFacingStatName.WIS:
+                    WISText.text = "" + RandomNumberInStatRange( upgradeManager.wisMin, upgradeManager.wisMax );
+                    break;
+                case PlayerFacingStatName.CON:
+                    CONText.text = "" + RandomNumberInStatRange( upgradeManager.conMin, upgradeManager.conMax );
+                    break;
+                case PlayerFacingStatName.CHA:
+                    CHAText.text = "" + RandomNumberInStatRange( upgradeManager.charismaMin, upgradeManager.charismaMax );
+                    break;
+            }
+        }
+        SetActualStatValues();
+    }
+
+    private int RandomNumberInStatRange(int min, int max)
+    {
+        return Random.Range(min, max+1);
     }
 }
