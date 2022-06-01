20fc21df837163f8a8e9bd65b76b49f7a7f72e06
portaljumperjen <portaljumperjen@gmail.com>
2022-06-01 12:05:35 -0700

    nick you need this stuff (got rid of AcitvatedItemData)

--- 169Capstone/Assets/Scripts/Shop/Shop.cs
+++ 169Capstone/Assets/Scripts/Shop/Shop.cs
@@ -23,8 +23,6 @@ public class Shop : MonoBehaviour
     {
         indexes = new HashSet<int>();
 
-        int tier = ObjectManager.bossesKilled;
-
         inventoryList = new List<RarityAssignmentTier>();
         inventory = new List<GeneratedEquipment>();
 
@@ -34,13 +32,23 @@ public class Shop : MonoBehaviour
         inventoryList.Add(shopKeeper.Slot4());
         inventoryList.Add(shopKeeper.Slot5());
 
+        // Most of the shop items generate as the CURRENT gear tier # value
+        int tier1 = GameManager.instance.gearTier;
+        // Fewer generate as that value + 1 (so Uncommon if gear tier = Common, etc.)
+        int tier2 = GameManager.instance.gearTier + 1;
+
+        // If Legendary, there's nothing above so make the less frequent version one tier down (Epic)
+        if( (ItemRarity)tier1 == ItemRarity.Legendary ){
+            tier2 = (int)ItemRarity.Epic;
+        }
+
         for(int i = 0; i < inventoryList.Count; i++){
             if(inventoryList[i] == RarityAssignmentTier.X){
                 // Generate a new item with rarity = tier #, then add it to the inventory HashSet
-                inventory.Add( GenerateItem((ItemRarity)tier) );
+                inventory.Add( GenerateItem((ItemRarity)tier1) );
             }
             else if(inventoryList[i] == RarityAssignmentTier.X_1){
-                inventory.Add( GenerateItem((ItemRarity)(tier+1)) );
+                inventory.Add( GenerateItem((ItemRarity)tier2) );
             }
         }
     }
