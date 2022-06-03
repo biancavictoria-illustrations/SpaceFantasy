857b64573458569852ce8f7ba5c904a5f5d5beb6
Alfamajor <alfamajor110@gmail.com>
2022-06-03 16:32:03 -0700

    Beetle Boss Projectile

--- 169Capstone/Assets/Scripts/Gear/VFXSpinner.cs
+++ 169Capstone/Assets/Scripts/Gear/VFXSpinner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class VFXSpinner : MonoBehaviour
 {
     [SerializeField] private float spinSpeed = 1;
+    [SerializeField] private float spinAxis = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,17 @@ public class VFXSpinner : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Rotate(0, spinSpeed, 0, Space.Self);
+        if(spinAxis == 1)
+        {
+            transform.Rotate(spinSpeed, 0,  0, Space.Self);
+        }
+        else if(spinAxis == 2)
+        {
+            transform.Rotate(0, spinSpeed, 0, Space.Self);
+        }
+        else
+        {
+            transform.Rotate(0,  0, spinSpeed, Space.Self);
+        }
     }
 }
