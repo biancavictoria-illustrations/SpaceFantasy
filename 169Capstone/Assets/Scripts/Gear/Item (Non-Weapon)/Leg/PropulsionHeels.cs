5d8252df93811768173ded05273460ebaa8dd17d
chaseoqueso <chaseoqueso@gmail.com>
2022-06-05 19:45:51 -0700

    Propulsion heels are complete

---
+++ 169Capstone/Assets/Scripts/Gear/Item (Non-Weapon)/Leg/PropulsionHeels.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropulsionHeels : Leg
+{
+    private const float dashBaseSpeed = 40f;
+
+    private Vector3 direction;
+    private Coroutine routine;
+    private CharacterController controller;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        controller = player.GetComponent<CharacterController>();
+    }
+
+    protected override void ActivateLegs()
+    {
+        base.ActivateLegs();
+
+        direction = controller.velocity.normalized;
+        InputManager.instance.preventInputOverride = true;
+        StartCoroutine(dashRoutine());
+    }
+
+    public override void ResetItemAndTriggerCooldown()
+    {
+        base.ResetItemAndTriggerCooldown();
+        
+        InputManager.instance.preventInputOverride = false;
+        StopCoroutine(routine);
+    }
+
+    public override void ManageCoroutinesOnUnequip()
+    {
+        base.ManageCoroutinesOnUnequip();
+
+        InputManager.instance.preventInputOverride = false;
+        StopCoroutine(routine);
+    }
+
+    private IEnumerator dashRoutine()
+    {
+        while(durationRoutine != null)
+        {
+            controller.Move(direction * dashBaseSpeed * player.stats.getMoveSpeed() * Time.fixedDeltaTime);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
