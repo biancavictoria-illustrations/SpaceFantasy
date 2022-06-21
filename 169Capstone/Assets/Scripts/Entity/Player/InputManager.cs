241c51472cd7602d7061dffd9aac8e2d7b8beb93
portaljumperjen <portaljumperjen@gmail.com>
2022-06-20 23:01:46 -0700

    change

--- 169Capstone/Assets/Scripts/Entity/Player/InputManager.cs
+++ 169Capstone/Assets/Scripts/Entity/Player/InputManager.cs
@@ -255,9 +255,11 @@ public class InputManager : MonoBehaviour
             InGameUIManager.instance.gearSwapUI.CloseGearSwapUI();
             return;            
         }
-        else if(GameManager.instance.statRerollUIOpen && GameManager.instance.currentRunNumber != 2){
-            InGameUIManager.instance.statRerollUI.DisableStatRerollUI();
-            return;
+        else if(GameManager.instance.statRerollUIOpen){
+            if(GameManager.instance.currentRunNumber != 2){
+                InGameUIManager.instance.statRerollUI.DisableStatRerollUI();
+                return;
+            }
         }
         else if(mapIsOpen){
             OnToggleMinimap(input);
